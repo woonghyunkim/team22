5b24ae488fc6ed485e77fbce10ea63e1181cd5ca
dh0369 <ggasa09@gmail.com>
2022-09-22 09:43:12 +0900

    dhdhdh

--- team22/src/main/java/com/multi/Test.java
+++ team22/src/main/java/com/multi/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		System.out.println("Test");
 		System.out.println("Test2");
 		System.out.println("manex3");
+		System.out.println("manex3");
+		System.out.println("manex3");
 		
 	}
 
